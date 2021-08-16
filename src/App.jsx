16d70ba9e7134bd47fe586ef1e72f2e38970f372
Noraluk <noraluk.kn@gmail.com>
2021-08-16 23:08:00 +0700

    :sparkles: add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import Header from '@componenets/header/header';
+import Banner from '@componenets/banner/banner';
 import './App.css';
 
 class App extends PureComponent {
@@ -7,6 +8,7 @@ class App extends PureComponent {
     return (
       <div>
         <Header />
+        <Banner />
       </div>
     );
   }
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import { Container, Row, Button } from 'react-bootstrap';
+import './banner.css';
+
+class Banner extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Container fluid className="Banner">
+          <Row style={{ textAlign: 'center' }}>
+            <p className="my-1" style={{ fontSize: '60px' }}>Punny headline</p>
+            <p className="my-0">And an even wittier subheading to boot. Jumpstart</p>
+            <p className="my-0">your marketing efforts with this example based on</p>
+            <p className="my-0">Apple’s marketing pages.</p>
+            <div>
+              <br />
+              <Button className="Banner-button" variant="outline-secondary" size="mg">Secondary</Button>
+            </div>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Banner;
