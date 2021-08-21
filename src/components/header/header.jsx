a81d8555ece93f0353de65319a8ddc52c64bee00
Noraluk <noraluk.kn@gmail.com>
2021-08-21 14:36:21 +0700

    :lipstick: update header ui

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import {
-  Navbar, Nav, NavDropdown,
+  Container, Navbar, Nav,
 } from 'react-bootstrap';
+import { AiFillAliwangwang } from 'react-icons/ai';
 
 import './header.css';
 
@@ -9,20 +10,23 @@ class Header extends PureComponent {
   render() {
     return (
       <div>
-        <Navbar bg="" expand="lg">
+        <Navbar bg="dark" expand="md" variant="dark">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="header">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+            <Container>
+              <Nav className="header">
+                <Navbar.Brand href="#home">
+                  <AiFillAliwangwang style={{ width: '30px', height: '30px', color: 'grey' }} />
+                </Navbar.Brand>
+                <Nav.Link href="#tour">Tour</Nav.Link>
+                <Nav.Link href="#product">Product</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#enterprise">Enterprise</Nav.Link>
+                <Nav.Link href="#support">Support</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#cart">Cart</Nav.Link>
+              </Nav>
+            </Container>
           </Navbar.Collapse>
         </Navbar>
       </div>
